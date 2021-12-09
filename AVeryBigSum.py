7a67417970080b6e66a50822ba7409e0a834c207
Raghav Verma <91414579+Raghav-Verma25@users.noreply.github.com>
2021-12-10 01:59:00 +0530

    Create AVeryBigSum.py
    In this challenge, you are required to calculate and print the sum of the elements in an array, keeping in mind that some of those integers may be quite large.

---
+++ AVeryBigSum.py
@@ -0,0 +1,25 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+def aVeryBigSum(ar):
+    # Write your code here
+    sum = 0
+    for i in ar:
+        sum = sum + i
+    return sum
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    ar_count = int(input().strip())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = aVeryBigSum(ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
