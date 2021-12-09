98bcae184b133d66b30365b7419f18578013493d
Raghav Verma <91414579+Raghav-Verma25@users.noreply.github.com>
2021-12-10 01:54:40 +0530

    Create SimpleArraySum.py
    Given an array of integers, find the sum of its elements.

---
+++ SimpleArraySum.py
@@ -0,0 +1,27 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+def simpleArraySum(ar):
+    # Write your code here
+    sum = 0 
+    for i in ar:
+        sum = sum + i
+    return sum
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    ar_count = int(input().strip())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = simpleArraySum(ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
